dbc2262da9bb6db8f081599ed5422a37ca6cfa03
Jkotoun <jkotoun58@gmail.com>
2023-03-07 19:23:51 +0100

    refactor

--- src/flp22-fun.hs
+++ src/flp22-fun.hs
@@ -26,25 +26,19 @@ knapsack ("-i" : input_file) = do
   case parsed_knapsack of
     Nothing -> error "parsing knapsack input failed"
     Just knapsack -> print knapsack
-
 knapsack ("-b" : input_file) = do
   parsed_knapsack <- parseInput input_file
   case parsed_knapsack of
     Nothing -> error "parsing knapsack input failed"
     Just knapsack -> bruteforce knapsack
-
 knapsack _ = error "invalid arg"
-solutionBitmap :: [Item] -> [Item] -> [Int]
-solutionBitmap allItems solutionItems =  map (\item -> if item `elem` solutionItems then 1 else 0) allItems
 
-printBitmap :: [Int] -> IO()
-printBitmap arr = print $ map (\char -> (if (char == ',') || (char == '"') then ' ' else char)) $ show arr
-
+--read knapsack input from file or stdin
 readInput :: [FilePath] -> IO String
 readInput [] =  getContents
 readInput [input_file] = readFile input_file
 
--- file arg can be empty
+-- parse knapsack input
 parseInput :: [FilePath] -> IO (Maybe Knapsack)
 parseInput args = do
   input <- readInput args
@@ -52,19 +46,21 @@ parseInput args = do
           Left _  -> Nothing
           Right x -> Just x)
 
+--map found solution to bitmap
+solutionBitmap :: [Item] -> [Item] -> [Int]
+solutionBitmap allItems solutionItems =  map (\item -> if item `elem` solutionItems then 1 else 0) allItems
 
--- bruteforce method
--- bruteforce :: Knapsack -> IO()
--- bruteforce :: Knapsack -> IO()
+--map bitmap int array to string format with spaces
+printBitmap :: [Int] -> IO()
+printBitmap arr = print $ map (\char -> (if (char == ',') || (char == '"') then ' ' else char)) $ show arr
 
+-- find solution using bruteforce solution (try all combinations, O(2^n))
+bruteforce :: Knapsack -> IO ()
 bruteforce (Knapsack maxWeight minCost items) = case bestSolution (0, Nothing) maxWeight minCost (subsequences items) of
   Nothing -> print False
   Just solution -> printBitmap $ solutionBitmap items solution
 
--- firstSolution :: Int -> Int -> [[Item]] -> Maybe [Item]
--- firstSolution _ _ [] = Nothing
--- firstSolution maxWeight minCost (x:xs) = if isValidSolution maxWeight minCost x then Just x else firstSolution maxWeight minCost xs
-
+-- get maybe best solution (nothing if solution does not exist)
 bestSolution ::  (Int, Maybe [Item]) -> Int -> Int -> [[Item]] -> Maybe [Item]
 bestSolution best _ _ [] = snd best
 bestSolution (currentBestCost, solutionItems)  maxWeight minCost (x:xs) = 
@@ -74,8 +70,7 @@ bestSolution (currentBestCost, solutionItems)  maxWeight minCost (x:xs) =
     then bestSolution (snd isValidAndBetterSolution, Just x) maxWeight minCost xs 
     else bestSolution (currentBestCost, solutionItems) maxWeight minCost xs
 
-
-
+-- check if given solution is valid and is better than current best found
 isValidAndBetter :: Int->Int -> Int -> [Item] -> (Bool, Int)
 isValidAndBetter currentBest maxWeight minCost items =
   let (totalWeight, totalCost) = foldl (\ (totalWeight, totalCost) (Item weight cost) -> (totalWeight + weight, totalCost + cost)) (0, 0) items
@@ -86,15 +81,6 @@ isValidAndBetter currentBest maxWeight minCost items =
 -- evolution algorithm method
 evolutionAlg :: Knapsack -> IO()
 evolutionAlg (Knapsack maxWeight minCost items) = putStrLn "evolutionAlg stdin"
-
-
-
-
-main :: IO ()
-main = do
-    args <- getArgs
-    knapsack args
-
 
 -- input file parser
 knapsackParser :: P.Parser Knapsack
@@ -133,3 +119,9 @@ item = do
   _ <- P.manyTill P.anyChar P.newline
   _ <- P.manyTill P.anyChar P.newline
   return (Item{weight=weight, cost=cost})
+
+
+main :: IO ()
+main = do
+    args <- getArgs
+    knapsack args
